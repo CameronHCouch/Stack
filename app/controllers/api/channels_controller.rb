89ee17d169a1c8477493278fd5317b6581ef6bd6
CameronHCouch <cameronhcouch@gmail.com>
2019-02-08 11:45:34 -0500

    Allow users to invite others to existing channels/DMs Note: components do not immediately update to reflect added user

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -31,13 +31,11 @@ class Api::ChannelsController < ApplicationController
   def update
     @channel = current_user.channels.find(params[:id])
     if @channel.update_attributes(channel_params)
-      if params[:channel][:member_list].length
-        invites = params[:channel][:member_list].split(" ")
+      if params[:channel]["member_list"].length
+        invites = params[:channel]["member_list"].split(" ")
         invites.each do |username|
           invited_user = User.find_by(username: username)
-          unless @channel.members.includes(invited_user)
-            Subscription.create!(user_id: invited_user.id, subscribable: @channel)
-          end
+          Subscription.create!(user_id: invited_user.id, subscribable: @channel)
         end
       end
       render :show
