8b56eb73eb8896ee4e0171f29d00eee0bff8e912
CameronHCouch <cameronhcouch@gmail.com>
2019-04-03 15:06:01 -0400

    Remove logged from production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,12 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
-import logger from 'redux-logger';
 
 export default (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(thunk)
   )
 )
