22e887940063958da01ad0c500ee750f9d3840b7
CameronHCouch <cameronhcouch@gmail.com>
2023-10-06 22:14:29 -0400

    Upgrade Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api, defaults: { format: JSON } do
-    resources :users, only: [:create, :show, :index]
-    resource :session, only: [:create, :destroy]
-    
-    resources :workspaces, only: [:index, :show, :create, :destroy]
-    resources :channels, only: [:index, :show, :create, :update, :destroy] do
-      resources :messages, only: [:index]
-    end
-  end
-
-  root to: 'static_pages#root'
-  mount ActionCable.server, at: '/cable'
 end
