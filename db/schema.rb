a0aa13cfa87193713c73d5b6942c79e2d6554b84
CameronHCouch <cameronhcouch@gmail.com>
2023-10-06 22:45:39 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_30_214655) do
-
+ActiveRecord::Schema[7.1].define(version: 2023_10_07_021054) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,8 +19,8 @@ ActiveRecord::Schema.define(version: 2019_01_30_214655) do
     t.string "description"
     t.boolean "is_dm", default: false, null: false
     t.integer "workspace_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["name"], name: "index_channels_on_name"
     t.index ["workspace_id"], name: "index_channels_on_workspace_id"
   end
@@ -30,8 +29,8 @@ ActiveRecord::Schema.define(version: 2019_01_30_214655) do
     t.string "body", null: false
     t.integer "author_id", null: false
     t.integer "channel_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["author_id"], name: "index_messages_on_author_id"
     t.index ["channel_id"], name: "index_messages_on_channel_id"
   end
@@ -39,8 +38,8 @@ ActiveRecord::Schema.define(version: 2019_01_30_214655) do
   create_table "subscriptions", force: :cascade do |t|
     t.integer "subscribable_id", null: false
     t.string "subscribable_type", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "user_id"
     t.index ["subscribable_id", "subscribable_type"], name: "index_subscriptions_on_subscribable_id_and_subscribable_type"
     t.index ["subscribable_id"], name: "index_subscriptions_on_subscribable_id"
@@ -52,8 +51,8 @@ ActiveRecord::Schema.define(version: 2019_01_30_214655) do
     t.string "username"
     t.string "password_digest", null: false
     t.string "session_token", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "img_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
@@ -62,8 +61,8 @@ ActiveRecord::Schema.define(version: 2019_01_30_214655) do
   create_table "workspaces", force: :cascade do |t|
     t.string "name", null: false
     t.integer "admin_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["admin_id"], name: "index_workspaces_on_admin_id"
     t.index ["name"], name: "index_workspaces_on_name", unique: true
   end
